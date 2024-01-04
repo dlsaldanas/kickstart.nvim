77f5a657800cf3774a849c4dbcf1bde4c829a730
dlsaldanas <dlsaldanas@falabella.cl>
2024-01-04 12:05:04 -0300

    last

--- init.lua
+++ init.lua
@@ -1,46 +1,3 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-
-Kickstart.nvim is *not* a distribution.
-
-Kickstart.nvim is a template for your own configuration.
-  The goal is that you can read every line of code, top-to-bottom, understand
-  what your configuration is doing, and modify it to suit your needs.
-
-  Once you've done that, you should start exploring, configuring and tinkering to
-  explore Neovim!
-
-  If you don't know anything about Lua, I recommend taking some time to read through
-  a guide. One possible example:
-  - https://learnxinyminutes.com/docs/lua/
-
-
-  And then you can explore or search through `:help lua-guide`
-  - https://neovim.io/doc/user/lua-guide.html
-
-
-Kickstart Guide:
-
-I have left several `:help X` comments throughout the init.lua
-You should run that command and read that help section for more information.
-
-In addition, I have some `NOTE:` items throughout the file.
-These are for you, the reader to help understand what is happening. Feel free to delete
-them once you know what you're doing, but they should serve as a guide for when you
-are first encountering a few different constructs in your nvim config.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now :)
---]]
-
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
@@ -254,18 +211,6 @@ require('lazy').setup({
     build = ':TSUpdate',
   },
 
-  -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
-  --       These are some example plugins that I've included in the kickstart repository.
-  --       Uncomment any of the lines below to enable them.
-  -- require 'kickstart.plugins.autoformat',
-  -- require 'kickstart.plugins.debug',
-
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
-  --    up-to-date with whatever is in the kickstart repo.
-  --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  --
-  --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
   { import = 'custom.plugins' },
 }, {})
 
@@ -321,7 +266,6 @@ vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = tr
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- copy current buffer path to "+
--- vim.keymap.set('n', '<leader>yb', '<cmd>let @+ = ' .. expand('%') .. '<cr>', {expr = true})
 vim.keymap.set('n', '<leader>yb', '<cmd>let @+ = expand("%")<CR>',
   { desc = 'yank to plus register current relative path' })
 -- Diagnostic keymaps
@@ -452,8 +396,6 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufNew", "BufWinEnter" }, {
   group = vim.api.nvim_create_augroup("ts_fold_workaround", { clear = true }),
   callback = function()
     vim.wo.foldmethod = "expr"
-    -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-    -- vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
     vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
     vim.wo.foldenable = false
   end,
@@ -531,8 +473,6 @@ end, 0)
 -- https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization
 local function goto_definition()
   local util = vim.lsp.util
-  local log = require("vim.lsp.log")
-
   -- note, this handler style is for neovim 0.5.1/0.6, if on 0.5, call with function(_, method, result)
   local handler = function(_, result, ctx)
     if result == nil or vim.tbl_isempty(result) then
@@ -558,7 +498,7 @@ local function goto_definition()
   return handler
 end
 
-vim.lsp.handlers["textDocument/definition"] = goto_definition()
+-- vim.lsp.handlers["textDocument/definition"] = goto_definition()
 vim.diagnostic.config({
   virtual_text = {
     source = "always", -- Or "if_many"
@@ -634,6 +574,7 @@ require('which-key').register({
 -- before setting up the servers.
 require('mason').setup()
 require('mason-lspconfig').setup()
+local nvim_lsp = require('lspconfig')
 
 -- Enable the following language servers
 --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -644,15 +585,15 @@ require('mason-lspconfig').setup()
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-  -- clangd = {},
-  -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
   tsserver = {
+    root_dir = nvim_lsp.util.root_pattern("package.json"),
     single_file_support = false
   },
-  eslint = {},
+  eslint = {
+    root_dir = nvim_lsp.util.root_pattern("package.json"),
+  },
   denols = {
+    root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc", "deno.json5", "deno.json6", "deno.json7", "deno.json8", "deno.json9", "deno.json10"),
     init_options = {
       lint = true,
       unstable = true,
@@ -695,12 +636,13 @@ mason_lspconfig.setup {
 
 mason_lspconfig.setup_handlers {
   function(server_name)
-    require('lspconfig')[server_name].setup {
+    nvim_lsp[server_name].setup {
       capabilities = capabilities,
       on_attach = on_attach,
       settings = servers[server_name],
       filetypes = (servers[server_name] or {}).filetypes,
       init_options = (servers[server_name] or {}).init_options,
+      root_dir = (servers[server_name] or {}).root_dir,
     }
   end,
 }
