3eaf1349b473cde30870c52cdb1a06909a0acd8c
dlsaldanas <dlsaldanas@falabella.cl>
2024-05-03 09:20:15 -0400

    latest

--- init.lua
+++ init.lua
@@ -9,7 +9,7 @@ vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 vim.wo.foldenable = false
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
